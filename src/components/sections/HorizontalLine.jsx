1fabcb3f5e379910979ce4ef1e70a6992a882691
AlexGrigorenco <alpsnhandy@gmail.com>
2023-02-19 08:54:55 +0200

    added gh-pages

--- src/components/sections/HorizontalLine.jsx
+++ src/components/sections/HorizontalLine.jsx
@@ -8,7 +8,7 @@ const HorizontalLine = ({position}) => {
 
 
     <div className={'absolute ' + position}>
-    <div className="bg-[#E7E5FF] h-[2px] w-[100vw] relative -z-1"></div>
+    <div className="bg-[#E7E5FF] h-[2px] w-[100vw] relative -z-1 "></div>
     </div>
 
 
